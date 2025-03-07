202b3f1c5e1a305f367c574e6870b7d444638b76
joaodunas <joaomariadonato@gmail.com>
2025-03-07 10:08:02 +0000

    added files needed for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ai-ssd.github.io/",
   vite: {
-      plugins: [tailwindcss()]
-	},
+    plugins: [tailwindcss()],
+  },
 
-  integrations: [react()]
+  integrations: [react()],
 });
